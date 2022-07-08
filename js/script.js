f4e5bcc85ff57de8901552c022b444da7202e021
Bell Cranel <lkenn125@gmail.com>
2022-07-08 17:10:50 +0300

    do exercise, learn object

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
 // let numberOfFilms;
 //
 // function start() {
-//     numberOfFilms = +prompt('How many films are u watched?', '')
+//     numberOfFilms = +prompt('How many films are u watched?', '').trim();
 //     while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
-//         numberOfFilms = +prompt('How many films are u watched?', '')
+//         numberOfFilms = +prompt('How many films are u watched?', '').trim();
 //     }
 // }
 // start();
@@ -19,8 +19,8 @@
 //
 // function rememberMyFilms() {
 //     for (let i = 0; i < 2; i++) {
-//         const a = prompt('One of the last viewed films?', ''),
-//               b = prompt('How rate u give for this film?', '');
+//         const a = prompt('One of the last viewed films?', '').trim(),
+//               b = prompt('How rate u give for this film?', '').trim();
 //         if ((a && b !== '') && (a && b !== null) && (a.length && b.length < 50)) {
 //             personalMovieDB.movies[a] = b;
 //         } else i--;
@@ -39,25 +39,27 @@
 // }
 // detectPersonalLevel();
 //
-// // function showMyDB() {
-// //     if (personalMovieDB.privat === false) {
-// //         console.log(personalMovieDB);
-// //     }
-// // }
-// // showMyDB();
-// ///////////////////////////////////////////////////////
 // function showMyDB(hidden) {
 //     if (!hidden) {
 //         console.log(personalMovieDB);
 //     }
 // }
 // showMyDB(personalMovieDB.privat);
+// //One more way
+// // function showMyDB() {
+// //     if (personalMovieDB.privat === false) {
+// //         console.log(personalMovieDB);
+// //     }
+// // }
+// // showMyDB();
 //
 // function writeYourGenres() {
 //     let answer;
 //     for (let i = 0; i < 3; i++) {
-//         answer = prompt(`Your favorite genre number ${i + 1}?`);
-//         personalMovieDB.genres.push(answer);
+//         answer = prompt(`Your favorite genre number ${i + 1}?`).trim();
+//         if (answer !== null && answer !== '') {
+//             personalMovieDB.genres.push(answer);
+//         } else i--;
 //
 //         // personalMovieDB.genres[i] = answer;
 //         // personalMovieDB.genres[i] = prompt(`Your favorite genre number ${i + 1}?`);
@@ -65,6 +67,9 @@
 // }
 // writeYourGenres();
 
+
+
+// Some exercise:
 // function getMathResult(num, base) {
 //     let result = '';
 //     if (typeof(base) !== 'number' || base === 0 || base < 0) {
@@ -80,116 +85,220 @@
 //     return result;
 // }
 // console.log(getMathResult(5, 3));
-
-// let result = '';
-// const length = 7;
 //
-// for (let i = 1; i < length; i++) {
-//     for (let j = 0; j < i; j++) {
-//         result += '*';
-//     }
-//     result += '\n';
-// }
-// console.log(result);
+// function starPyramidBuilding() {
+//     let result = '';
+//     const length = 7;
 //
-
-// const lines = 6;
-// let result = '';
-// let emptySpace = ' '
-// // Проверяется именно переменная result, формируйте строку в ней
-// for (let i = 1; i <= lines ; i++) {
-//     for (let j = 0; j < i + 2 ; j++) {
-//         if (result == lines) {
-//             result = result - lines + '*';
+//     for (let i = 1; i < length; i++) {
+//         for (let j = 0; j < i; j++) {
+//             result += '*';
 //         }
-//         result += '*';
+//         result += '\n';
 //     }
-//     result += '\n';
+//     return result;
 // }
-// console.log(result);
-
-function calculateVolumeAndArea(num) {
-    if (typeof(num) === 'string' || num === null || Math.sign(num) !== 1 || Number.isInteger(num) !== true) {
-        return 'Error occurred';
-    } else {
-        let volume = Math.pow(num, 3),
-            area = 6 * Math.pow(num, 2);
-        // console.log(`Volume = ${volume}, area = ${area}`);
-        return `Обьем куба: ${volume} площадь всей поверхности: ${area}`.toString();
-    }
-}
-calculateVolumeAndArea(5);
-
-function getCoupeNumber(place) {
-    if (place === 0 || place >= 37) {
-        return 'There are no such places';
-    }
-    if (typeof(place) === 'string' || place === null || Math.sign(place) !== 1 || Number.isInteger(place) !== true) {
-        return 'Incorrect place';
-    }
-    else {
-        place = Math.ceil(place/4);
-        // row = Math.pow(place, 1/2);
-        // coupe = Math.floor(row);
-        // return `Your row is number ${coupe}`;
-        return place;
-    }
-}
-console.log((getCoupeNumber(36)));
-
-// function showMessage() {
+// console.log(starPyramidBuilding());
 //
+// //Incorrect
+// function doubleStarPyramidBuilding() {
+//     const lines = 6;
+//     let result = '';
+//     let emptySpace = ' '
+// // Проверяется именно переменная result, формируйте строку в ней
+//     for (let i = 1; i <= lines ; i++) {
+//         for (let j = 0; j < i + 2 ; j++) {
+//             if (result == lines) {
+//                 result = result - lines + '*';
+//             }
+//             result += '*';
+//         }
+//         result += '\n';
+//     }
+//     return result;
+// }
+// console.log(doubleStarPyramidBuilding());
+//
+// function calculateVolumeAndArea(num) {
+//     if (typeof(num) === 'string' || num === null || Math.sign(num) !== 1 || Number.isInteger(num) !== true) {
+//         return 'Error occurred';
+//     } else {
+//         let volume = Math.pow(num, 3),
+//             area = 6 * Math.pow(num, 2);
+//         // console.log(`Volume = ${volume}, area = ${area}`);
+//         return `Обьем куба: ${volume} площадь всей поверхности: ${area}`.toString();
+//     }
+// }
+// console.log(calculateVolumeAndArea(5));
+//
+// function getCoupeNumber(place) {
+//     if (place === 0 || place >= 37) {
+//         return 'There are no such places';
+//     }
+//     if (typeof(place) === 'string' || place === null || Math.sign(place) !== 1 || Number.isInteger(place) !== true) {
+//         return 'Incorrect place';
+//     }
+//     else {
+//         place = Math.ceil(place/4);
+//         // row = Math.pow(place, 1/2);
+//         // coupe = Math.floor(row);
+//         // return `Your row is number ${coupe}`;
+//         return place;
+//     }
+// }
+// console.log((getCoupeNumber(36)));
+//
+// function getTimeFromMinutes(minutes) {
+//     if (typeof(minutes) !== 'number' || Number.isInteger(minutes) !== true || minutes < 0) {
+//         return 'Incorrect time';
+//     }
+//     let min = minutes % 60;
+//     let hours = Math.trunc(minutes / 60);
+//     // Original (english version)
+//     if (hours <= 1) {
+//         return `This is ${hours} hour and ${min} minutes`;
+//     } else {
+//         return `This is ${hours} hours and ${min} minutes`;
+//     }
+//     //Russian version
+//     // if (hours === 0) {
+//     //     return `Это ${hours} часов и ${min} минут`;
+//     // }
+//     // if (hours === 1) {
+//     //     return `Это ${hours} час и ${min} минут`;
+//     // }
+//     // if (hours > 1 || hours < 5) {
+//     //     return `Это ${hours} часа и ${min} минут`;
+//     // }
+//     // if(hours > 5) {
+//     //     return `Это ${hours} часов и ${min} минут`;
+//     // }
+// }
+// console.log(getTimeFromMinutes(210));
+//
+// function findMaxNumber(...arguments) {
+//     if (arguments.some((i) => Number.isNaN(i) || typeof(i) !== 'number')) {
+//         return 0;
+//     } else return Math.max(...arguments);
+//
+//     // if (array.find((i) => Number.isNaN(i) || typeof(i) !== 'number')) {
+//     //     return 0
+//     // } else return Math.max(...array);
+//     //
+//     // if (array.find((i) => typeof(i) === 'number' && Number.isNaN(i)) !== true && array.length >= 4) {
+//     //     return Math.max(...array);
+//     // } else return 0;
+//     //
+//     //
+//     // if (isNaN(array.find(i => isNaN(i) || typeof(i) === 'object'))) {
+//     //     return 0
+//     // }
+//     // if (array.find((item) => typeof(item) === 'string' || array.length < 4)) {
+//     //     return 0;
+//     // } else ;
+// }
+// console.log(findMaxNumber(4, 5.5, 6, []));
+//
+// function fibonacciNumber(num) {
+//     let result = [];
+//     if (typeof(num) !== 'number' || Number.isInteger(num) !== true) {
+//         return '';
+//     }
+//     for (let i = 0; i < num; i++ ) {
+//         result[i] = i;
+//         if (i > 1) {
+//             result[i] = result[i - 1] + result[i - 2];
+//         }
+//     }
+//     return result.join(' ');
+// }
+// console.log(fibonacciNumber(15));
+
+//Fibonacci another way (without array)
+// function fib(num) {
+//     if (typeof(num) !== 'number' || num <= 0 || !Number.isInteger(num)) {
+//         return "";
+//     }
+//
+//     let result = '';
+//     let first = 0;
+//     let second = 1;
+//
+//     for (let i = 0; i < num; i++) {
+//         if (i + 1 === num) {
+//             result += `${first}`;
+//             // Без пробела в конце
+//         } else {
+//             result += `${first} `;
+//         }
+//
+//         let third = first + second;
+//         first = second;
+//         second = third;
+//     }
+//
+//     return result;
+// }
+//
+// fib(5)
+
+//Callback                       функция как аргумент
+// function testCallback(string, callbackFunction) {
+//     const a = prompt(string, '');
+//     //Проверку конечно лучше сделать здесь, а не отдельной функцией
+//     //Но для примера подойдёт
+//
+//     //     Функция вызывается
+//     return callbackFunction(a);
+// }
+// //Функция передаётся аргументом (т.е без скобочек), а потом уже выхывается внутри другой - callback
+// console.log(testCallback('Enter something', someFunctionsForTest));
+// function someFunctionsForTest(str) {
+//     if (str !== null && str !== '') {
+//         return str;
+//     } else return testCallback('Enter something', someFunctionsForTest);
 // }
 
-// Math.sign(minutes) !== 1
-function getTimeFromMinutes(minutes) {
-    if (typeof(minutes) !== 'number' || Number.isInteger(minutes) !== true || minutes < 0) {
-        return 'Incorrect time';
-    }
-    let min = minutes % 60;
-    let hours = Math.trunc(minutes / 60);
-    //Original (english version)
-    // if (hours <= 1) {
-    //     return `This is ${hours} hour and ${min} minutes`;
-    // } else {
-    //     return `This is ${hours} hours and ${min} minutes`;
-    // }
+//Object
+// const options = {
+//     name: 'test',
+//     width: 1024,
+//     height: 1024,
+//     colors: {
+//         border: 'red',
+//         bg: 'black'
+//     },
+//     makeTest: function () {
+//         console.log('test');
+//     }
+// };
+// options.makeTest();
+//
+// //Деструктуризация обьектов:
+// const {border, bg} =  options.colors;
+// //Теперь это отдельные переменные
+// console.log(border);
+//
+// let counter = 0;
+// for (let key in options) {
+//     if (typeof(options[key]) === 'object') {
+//         for (let i in options[key]) {
+//             counter++;
+//         }
+//     } else counter++;
+// }
+// console.log(counter);
+//
+// for (let key in options) {
+//     if (typeof(options[key]) === 'object') {
+//         for (let i in options[key]) {
+//             console.log(`Property ${i} is ${options[key][i]}`);
+//         }
+//     } else console.log(`Property ${key} is ${options[key]}`);
+// }
 
-    //Russian version
-    // if (hours === 0) {
-    //     return `Это ${hours} часов и ${min} минут`;
-    // }
-    // if (hours === 1) {
-    //     return `Это ${hours} час и ${min} минут`;
-    // }
-    // if (hours > 1 || hours < 5) {
-    //     return `Это ${hours} часа и ${min} минут`;
-    // }
-    // if(hours > 5) {
-    //     return `Это ${hours} часов и ${min} минут`;
-    // }
-}
-console.log(getTimeFromMinutes(210));
-// || array.length < 4
-function findMaxNumber(...arguments) {
-    if (arguments.some((i) => Number.isNaN(i) || typeof(i) !== 'number')) {
-        return 0;
-    } else return Math.max(...arguments);
 
-    // if (array.find((i) => Number.isNaN(i) || typeof(i) !== 'number')) {
-    //     return 0
-    // } else return Math.max(...array);
-    //
-    // if (array.find((i) => typeof(i) === 'number' && Number.isNaN(i)) !== true && array.length >= 4) {
-    //     return Math.max(...array);
-    // } else return 0;
-    //
-    //
-    // if (isNaN(array.find(i => isNaN(i) || typeof(i) === 'object'))) {
-    //     return 0
-    // }
-    // if (array.find((item) => typeof(item) === 'string' || array.length < 4)) {
-    //     return 0;
-    // } else ;
-}
-console.log(findMaxNumber(4, 5.5, 6, []));
+let arrayTest = [1, 2, 3, 99];
+arrayTest.forEach((i, index) => console.log(`Element ${index} is ${i}`));
+arrayTest.forEach((i, index) => console.log(i * index));
+console.log(arrayTest);
